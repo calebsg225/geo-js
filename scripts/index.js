ac2d4a75422be522b71b4562d093dc479bcbd53b
calebsg225 <calebsg2253284@gmail.com>
2025-01-25 01:08:13 -0500

    canvas resizing
    The canvas element is now liquid :)
    - on load, canvas takes up entire client browser window
    - on resize, canvas updates to fit new client browser window

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,33 @@
 const body = document.querySelectorAll('body')[0];
 
-const message = "HTML, JS, CSS connects";
 body.innerHTML = `
 	<div id="main" >
-		${message}
+		<canvas id="geo-canvas"></canvas>
+		<div id="interface"></div>
 	</div>
 `;
+
+let width = body.clientWidth;
+let height = body.clientHeight;
+
+const geoCanvas = document.querySelector('#geo-canvas');
+geoCanvas.width = width;
+geoCanvas.height = height;
+const ctx = geoCanvas.getContext('2d');
+
+window.addEventListener('resize', () => {
+	width = body.clientWidth;
+	height = body.clientHeight;
+	geoCanvas.width = width;
+	geoCanvas.height = height;
+	render();
+});
+
+const render = () => {
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, width, height);
+	ctx.fillStyle = "red";
+	ctx.font = "2rem serif";
+	ctx.fillText(`${width}, ${height}`, 10, 30);
+}
+render();
---
+++ scripts/render.js
@@ -0,0 +1,5 @@
+/**
+ * render.js
+ *
+ * project 3d graph elements onto a 2d HTMLCanvasElement
+ */
