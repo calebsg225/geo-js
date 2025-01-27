22eec9c5e05778cc597599259b50ff114400f6d2
calebsg225 <calebsg2253284@gmail.com>
2025-01-27 13:55:09 -0500

    Create 3d object structure classes
    - Node class creates a new Node
    - Edge class creates a new Edge between 2 Nodes
    - Face class creates a new Face between 3 Nodes

---
+++ scripts/structures.js
@@ -0,0 +1,111 @@
+import { calc3dDistance, calcTriangleArea } from "./util";
+
+/**
+ * Creates a new Node
+ * @class
+ */
+class Node {
+
+	/**
+	 * builds initial node state
+	 * @constructor
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {number} z
+	 * @param {string} name
+	 */
+	constructor(x, y, z, name) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.edges = [];
+		this.faces = [];
+		this.name = name;
+	}
+
+	/**
+	 * replaces old Node coordinates with new Coordinates
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {number} z
+	 */
+	updateCoord(x, y, z) {
+		// TODO: return whether node switches near/far
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * adds an edge connection to Node
+	 * @param {string} edge
+	 */
+	addEdge(edge) {
+		this.edges.push(edge);
+	}
+
+	/**
+	 * adds a face connection to Node
+	 * @param {string} face
+	 */
+	addFace(face) {
+		this.faces.push(face);
+	}
+}
+
+/**
+ * Creates a new Edge
+ * @class
+ */
+class Edge {
+	/**
+	* builds initial node state
+	* @constructor
+	* @param {Node} node1 
+	* @param {Node} node2 
+	*/
+	constructor(node1, node2) {
+		this.nodes = [];
+		this.nodes.push(node1.name, node2.name);
+		this.length = calc3dDistance(
+			node1.x,
+			node1.y,
+			node1.z,
+			node2.x,
+			node2.y,
+			node2.z
+		);
+	}
+
+}
+
+/**
+ * Creates a new Face
+ * @class
+ */
+class Face {
+	/**
+	* build a new Face
+	* @constructor
+	* @param {Node} node1 
+	* @param {Node} node2 
+	* @param {Node} node3 
+	*/
+	constructor(node1, node2, node3) {
+		this.nodes = [];
+		this.nodes.push(node1.name, node2.name, node3.name);
+		this.area = calcTriangleArea(
+			node1.x,
+			node1.y,
+			node1.z,
+			node2.x,
+			node2.y,
+			node2.z,
+			node3.x,
+			node3.y,
+			node3.z,
+		);
+	}
+}
+
+export { Node, Edge, Face };
--- scripts/util.js
+++ scripts/util.js
@@ -1,5 +1,59 @@
+
 /**
- * util.js
- *
- * 
+ * calculates the distance between two Nodes
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @param {number} dx
+ * @param {number} dy
+ * @param {number} dz
+ * @return {number}
  */
+const calc3dDistance = (x, y, z, dx, dy, dz) => {
+	return Math.sqrt((dx - x) ** 2 + (dy - y) ** 2 + (dz - z) ** 2);
+}
+
+/**
+ * calculates the area of a triangle given 3 node positions
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} z1
+ * @param {number} x2
+ * @param {number} y2
+ * @param {number} z2
+ * @param {number} x3
+ * @param {number} y3
+ * @param {number} z3
+ * @returns {number}
+ */
+const calcTriangleArea = (
+	x1,
+	y1,
+	z1,
+	x2,
+	y2,
+	z2,
+	x3,
+	y3,
+	z3
+) => {
+	// vector from 1 to 2
+	const v12x = x2 - x1;
+	const v12y = y2 - y1;
+	const v12z = z2 - z1;
+
+	// vector from 1 to 3
+	const v13x = x3 - x1;
+	const v13y = y3 - y1;
+	const v13z = z3 - z1;
+
+	// orthogonal vector from v12 and v13
+	const ovx = v12y * v13z - v13y * v12z;
+	const ovy = v12x * v13z - v13x * v12z;
+	const ovz = v12x * v13y - v13x * v12y;
+
+	// return area
+	return Math.sqrt(ovx ** 2 + ovy ** 2 + ovz ** 2) / 2;
+}
+
+export { calc3dDistance, calcTriangleArea };
