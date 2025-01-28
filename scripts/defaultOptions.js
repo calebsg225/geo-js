729372111ce1066eff3b9217c82f8cbf452466a1
calebsg225 <calebsg2253284@gmail.com>
2025-01-27 23:36:40 -0500

    add default options
    - defaultOptions.js: contains default configuration for structure
      colors, sizing, shape, etc.

---
+++ scripts/defaultOptions.js
@@ -0,0 +1,41 @@
+const defaultOptions = {
+	backgroundColor: "black",
+	baseShape: "icosahedron",
+	frequency: 5,
+	base: {
+		near: {
+			nodes: {
+				show: true,
+				color: "#FFFFFF",
+				size: 3,
+			},
+			edges: {
+				show: true,
+				color: "#FFFFFF",
+				size: 3,
+			},
+			faces: {
+				show: true,
+				color: "#FFFFFF",
+			}
+		},
+		far: {
+			nodes: {
+				show: true,
+				color: "#FFFFFF",
+				size: 3,
+			},
+			edges: {
+				show: true,
+				color: "#FFFFFF",
+				size: 3,
+			},
+			faces: {
+				show: true,
+				color: "#FFFFFF",
+			}
+		}
+	},
+};
+
+export default defaultOptions;
