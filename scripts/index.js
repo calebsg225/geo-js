6147eaac35565eb0ea308b327635fda2948b57b4
calebsg225 <calebsg2253284@gmail.com>
2025-01-27 23:38:33 -0500

    put render function into its own class in another file
    - this is in preparation for additional functionality required to render
      complex 3d structures

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 import { buildIcosahedronAtFrequency } from "./geodesic.js";
+import Renderer from "./render.js";
+import defaultOptions from "./defaultOptions.js";
 
 const body = document.querySelectorAll('body')[0];
 
@@ -15,23 +17,17 @@ let height = body.clientHeight;
 const geoCanvas = document.querySelector('#geo-canvas');
 geoCanvas.width = width;
 geoCanvas.height = height;
-const ctx = geoCanvas.getContext('2d');
 
+const renderer = new Renderer(geoCanvas, defaultOptions);
+
+// update canvas width and height to match client window size
 window.addEventListener('resize', () => {
 	width = body.clientWidth;
 	height = body.clientHeight;
 	geoCanvas.width = width;
 	geoCanvas.height = height;
-	render();
+	renderer.render();
 });
-
-const render = () => {
-	ctx.fillStyle = "black";
-	ctx.fillRect(0, 0, width, height);
-	ctx.fillStyle = "red";
-	ctx.font = "2rem serif";
-	ctx.fillText(`${width}, ${height}`, 10, 30);
-}
-render();
+renderer.render();
 
 console.log(buildIcosahedronAtFrequency(5));
--- scripts/render.js
+++ scripts/render.js
@@ -1,5 +1,39 @@
+
 /**
- * render.js
- *
- * project 3d graph elements onto a 2d HTMLCanvasElement
+ * manages render
+ * @class
  */
+class Renderer {
+
+	/**
+	 * @constructor
+	 * @param {HTMLCanvasElement} canvas 
+	 */
+	constructor(canvas, options) {
+		this.canvas = canvas;
+		this.options = options;
+		this.ctx = canvas.getContext('2d');
+	}
+
+	/** 
+	 * clears and renders inputed data
+	 */
+	render = () => {
+		this.clearCanvas();
+
+		this.showCanvasDim();
+	}
+
+	clearCanvas = () => {
+		this.ctx.fillStyle = this.options.backgroundColor;
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+
+	showCanvasDim = () => {
+		this.ctx.fillStyle = "red";
+		this.ctx.font = "2rem serif";
+		this.ctx.fillText(`${this.canvas.width}, ${this.canvas.height}`, 10, 30);
+	}
+}
+
+export default Renderer;
